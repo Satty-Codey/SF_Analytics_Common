5e04ecc5a0f8001e135434a6663a2bcb9f711df7
Satty-Codey <satty.codey@gmail.com>
2025-02-09 19:43:46 +0530

    Create LocalNetworkPerformanceService.js

---
+++ Common/src/api/network/LocalNetworkPerformanceService.js
@@ -0,0 +1,24 @@
+//@flow
+import AjaxService from 'Common/src/ajax/AjaxService.service.js';
+import type {NetworkJson, NetworkPerformanceService} from 'Common/src/api/network/NetworkPerformanceService.service.js';
+
+/**
+ * REST version of the API for Network Details.
+ * Upload speed, download speed, latency, octane score
+ */
+export class LocalNetworkPerformanceService implements NetworkPerformanceService {
+	async getDownloadSpeed(size: number): Promise<NetworkJson> {
+		const startTime = performance.now();
+		return Promise.resolve(AjaxService.fetch('/WaveCommon/repo/dashboards/DashboardInspector_NetworkAsset.json')).then(
+			() => {
+				const duration = performance.now() - startTime;
+				return {
+					speed: Number.parseFloat(((size * 8) / duration).toFixed(2)),
+					duration
+				};
+			}
+		);
+	}
+}
+
+export default (new LocalNetworkPerformanceService(): LocalNetworkPerformanceService);
